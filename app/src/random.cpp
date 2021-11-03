b7f93052b280bbf82adada78fcba109e290f13b7
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-11-03 15:01:56 +0100

    Faster random generator

--- app/src/random.cpp
+++ app/src/random.cpp
@@ -40,61 +40,192 @@ namespace MaxFlow::App
 		}
 	}
 
+	std::vector<size_t> randomCounts(size_t _verticesCount, size_t _edgesCount, double _backwardsEdgeDensityFactor, unsigned int _seed)
+	{
+		std::mt19937 generator{ _seed };
+		std::vector<size_t> counts(_verticesCount);
+		size_t countSum{};
+		{
+			std::vector<double> factors(_verticesCount);
+			double sum{};
+			{
+				const std::uniform_real_distribution<double> distribution{ 0, 1 };
+				for (size_t i{}; i < _verticesCount; i++)
+				{
+					const double backwardFactor{ static_cast<double>(i) / (_verticesCount - 1) };
+					const double weight{ _backwardsEdgeDensityFactor * backwardFactor + (1 - backwardFactor) };
+					sum += factors[i] = distribution(generator) * weight;
+				}
+				if (!sum)
+				{
+					std::uniform_int_distribution<size_t> altDistribution{ 0, _verticesCount - 1 };
+					sum = factors[altDistribution(generator)] = 1;
+				}
+			}
+			for (size_t i{}; i < _verticesCount; i++)
+			{
+				countSum += counts[i] = std::min(_verticesCount - 1, static_cast<size_t>(std::round(factors[i] / sum * _edgesCount)));
+			}
+		}
+		if (countSum != _edgesCount)
+		{
+			const bool less{ countSum < _edgesCount };
+			auto isTarget{ [less, max = _verticesCount - 1] (size_t _count) {return less ? _count < max : _count > 0; } };
+			std::vector<size_t> targets;
+			targets.reserve(_verticesCount);
+			for (size_t i{}; i < _verticesCount; i++)
+			{
+				if (isTarget(counts[i]))
+				{
+					targets.push_back(i);
+				}
+			}
+			while (countSum != _edgesCount)
+			{
+				std::uniform_int_distribution<size_t> distribution{ 0, targets.size() - 1 };
+				size_t targetIndex{ distribution(generator) };
+				size_t& target{ counts[targets[targetIndex]] };
+				if (less)
+				{
+					target++;
+					countSum++;
+				}
+				else
+				{
+					target--;
+					countSum--;
+				}
+				if (!isTarget(target))
+				{
+					std::swap(targets[targetIndex], targets[targets.size() - 1]);
+					targets.pop_back();
+				}
+			}
+		}
+		size_t c{};
+		for (size_t x : counts) c += x;
+		if (c != _edgesCount)
+		{
+			throw std::runtime_error{ "" };
+		}
+		return counts;
+	}
+
+	class Picker final
+	{
+
+	private:
+
+		std::vector<size_t> m_data{};
+		std::vector<size_t> m_undo{};
+		size_t m_size{};
+
+	public:
+
+		Picker(size_t _count)
+		{
+			m_data.resize(_count);
+			m_undo.reserve(_count);
+			for (size_t i{ }; i < _count; i++)
+			{
+				m_data[i] = i;
+			}
+		}
+
+		void reset(size_t _count)
+		{
+			for (const size_t undo : m_undo)
+			{
+				m_data[undo] = undo;
+			}
+			const size_t oldCount{ m_size + m_undo.size() };
+			while (m_size < oldCount)
+			{
+				m_data[m_size] = m_size;
+				m_size++;
+			}
+			m_undo.clear();
+			m_size = _count;
+		}
+
+		size_t pop(std::mt19937& _engine)
+		{
+			if (empty())
+			{
+				throw std::logic_error{ "empty" };
+			}
+			std::uniform_int_distribution<size_t> distribution{ 0, m_size - 1 };
+			std::swap(m_data[distribution(_engine)], m_data[m_size - 1]);
+			m_size--;
+			const size_t result{ m_data[m_size] };
+			m_undo.push_back(result);
+			return result;
+		}
+
+		bool empty() const
+		{
+			return !m_size;
+		}
+
+	};
+
 	RandomProblem::RandomProblem(const RandomParameters& _parameters, unsigned int _seed) : m_graph{}
 	{
 		_parameters.validate();
 		m_graph.addVertices(_parameters.verticesCount);
-		std::mt19937 generator{ _seed };
 		std::vector<double> capacities(_parameters.edgesCount);
 		{
-			constexpr double maxExponent{ 3 };
-			const double exponentAbs{ 1 + std::abs(_parameters.capacityDeviance) * (maxExponent - 1) };
-			const double exponent{ _parameters.capacityDeviance < 0 ? exponentAbs : 1 / exponentAbs };
-			const std::uniform_real_distribution<double> distribution{ 0,1 };
-			double maxAlpha{};
-			for (size_t i{}; i < _parameters.edgesCount; i++)
+			// Capacities
+			std::mt19937 generator{ _seed };
 			{
-				capacities[i] = distribution(generator);
-				if (capacities[i] > maxAlpha)
+				constexpr double maxExponent{ 3 };
+				const double exponentAbs{ 1 + std::abs(_parameters.capacityDeviance) * (maxExponent - 1) };
+				const double exponent{ _parameters.capacityDeviance < 0 ? exponentAbs : 1 / exponentAbs };
+				const std::uniform_real_distribution<double> distribution{ 0,1 };
+				double maxAlpha{};
+				for (size_t i{}; i < _parameters.edgesCount; i++)
 				{
-					maxAlpha = capacities[i];
+					capacities[i] = distribution(generator);
+					if (capacities[i] > maxAlpha)
+					{
+						maxAlpha = capacities[i];
+					}
+				}
+				if (!maxAlpha && _parameters.edgesCount)
+				{
+					maxAlpha = capacities[0] = 1;
+				}
+				for (size_t i{}; i < _parameters.edgesCount; i++)
+				{
+					capacities[i] = std::pow(capacities[i] / maxAlpha, exponent) * (_parameters.maxCapacity - 1) + 1;
 				}
 			}
-			if (!maxAlpha && _parameters.edgesCount)
-			{
-				maxAlpha = capacities[0] = 1;
-			}
-			for (size_t i{}; i < _parameters.edgesCount; i++)
-			{
-				capacities[i] = std::pow(capacities[i] / maxAlpha, exponent) * (_parameters.maxCapacity - 1) + 1;
-			}
 		}
-		std::vector<std::tuple<size_t, size_t>> forwardEdgePool;
-		std::vector<std::tuple<size_t, size_t>> backwardEdgePool;
-		forwardEdgePool.reserve(_parameters.verticesCount * (_parameters.verticesCount - 1) / 2);
-		backwardEdgePool.reserve(_parameters.verticesCount * (_parameters.verticesCount - 1) / 2);
-		for (size_t a{}; a < _parameters.verticesCount; a++)
 		{
-			for (size_t b{}; b < _parameters.verticesCount; b++)
+			// Topology
+			std::mt19937 generator{ _seed };
+			const std::vector<size_t> counts{ randomCounts(_parameters.verticesCount, _parameters.edgesCount, _parameters.backwardsEdgeDensityFactor, _seed) };
+			Picker forwardPicker{ _parameters.verticesCount - 1 }, backwardPicker{ _parameters.verticesCount - 1 };
+			std::bernoulli_distribution backwardDistribution{ _parameters.backwardsEdgeDensityFactor };
+			for (FlowVertex& vertex : m_graph)
 			{
-				if (a == b)
+				forwardPicker.reset(_parameters.verticesCount - vertex.index() - 1);
+				backwardPicker.reset(vertex.index());
+				for (size_t c{}; c < counts[vertex.index()]; c++)
 				{
-					continue;
+					size_t to;
+					if (!backwardPicker.empty() && (forwardPicker.empty() || backwardDistribution(generator)))
+					{
+						to = backwardPicker.pop(generator);
+					}
+					else
+					{
+						to = forwardPicker.pop(generator) + vertex.index() + 1;
+					}
+					const Graphs::flow_t capacity{ std::clamp<Graphs::flow_t>(static_cast<Graphs::flow_t>(std::round(capacities[m_graph.edgesCount()])), 1, _parameters.maxCapacity) };
+					vertex.addOutEdge(to, { capacity });
 				}
-				std::vector<std::tuple<size_t, size_t>>& pool{ b > a ? forwardEdgePool : backwardEdgePool };
-				pool.push_back({ a, b });
 			}
-		}
-		std::bernoulli_distribution fbDistribution{ _parameters.backwardsEdgeDensityFactor };
-		while (m_graph.edgesCount() < _parameters.edgesCount)
-		{
-			std::vector<std::tuple<size_t, size_t>>& pool{ !backwardEdgePool.empty() && (forwardEdgePool.empty() || fbDistribution(generator)) ? backwardEdgePool : forwardEdgePool };
-			const std::uniform_int_distribution<size_t> poolDistribution{ 0, pool.size() - 1 };
-			std::swap(pool[pool.size() - 1], pool[poolDistribution(generator)]);
-			const std::tuple<size_t, size_t> edge{ pool[pool.size() - 1] };
-			pool.pop_back();
-			const Graphs::flow_t capacity{ std::clamp<Graphs::flow_t>(static_cast<Graphs::flow_t>(std::round(capacities[m_graph.edgesCount()])), 1, _parameters.maxCapacity) };
-			m_graph[std::get<0>(edge)].addOutEdge(std::get<1>(edge), { capacity });
 		}
 	}
 
